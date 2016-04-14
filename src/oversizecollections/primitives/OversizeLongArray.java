13876ef61b2e413865d8f3beeb71c1cc16438dfb
Matthew Hebert <soyrux@gmail.com>
2016-04-13 21:11:56 -0400

    Add Oversize arrays for Bytes, Longs, and Shorts

--- src/oversizecollections/primitives/OversizeLongArray.java
+++ src/oversizecollections/primitives/OversizeLongArray.java
@@ -1,22 +1,31 @@
-package oversizecollections;
+package oversizecollections.primitives;
 
 /**
- * Array of long size
+ * This class will generate an easily usable array that is larger than the maximum allowable
+ * array in the JVM. (Roughly Integer.MAX_VALUE - 8, slightly higher in some versions.)
+ *
+ * Indexes are longs (64-bit) and elements are longs (64-bit).
+ *
+ * Be sure to make sure that your heap size is adequate, because this class uses memory extremely easily
+ * if you're not careful. If -Xmx/-Xms flags are not giving you sufficient heap, also add -d64, which
+ * puts your JVM in explicit 64-bit mode.
+ *
+ * @author Ellen Hebert
  */
-public final class OversizeArray<E> implements OversizeList<E> {
+public final class OversizeLongArray {
 
     /**
-     * The size of the oversizecollections.OversizeArray in terms of array elements.
+     * The size of the OversizeIntArray in terms of array elements.
      * Specified when calling constructor.
      */
     private long size;
 
     /**
-     * The 'segments' of the oversizecollections.OversizeArray. Since there is a cap on the size of a single
+     * The 'segments' of the OversizeIntArray. Since there is a cap on the size of a single
      * array, this class makes a number of separate arrays then does the math to make them
      * appear to be one externally.
      */
-    private E[][] segments;
+    private long[][] segments;
 
     /**
      * Biggest array size guaranteed to work across all JVMs.
@@ -24,20 +33,20 @@ public final class OversizeArray<E> implements OversizeList<E> {
     private static final int MAX_ARR_SIZE = Integer.MAX_VALUE - 8;
 
     /**
-     * Constructs a oversizecollections.OversizeArray.
+     * Constructs a oversizecollections.OversizeIntArrayves.OversizeIntArray.
      * @param size the number of array elements
      */
-    public OversizeArray(final long size) {
+    public OversizeLongArray(final long size) {
         this.size = size;
 
         int segmentCount = (int) (size / MAX_ARR_SIZE) + 1;
-        segments = (E[][]) new Object[segmentCount][];
+        segments = new long[segmentCount][];
 
         for (int i = 0; i < segmentCount - 1; i++) {
-            segments[i] = (E[]) new Object[MAX_ARR_SIZE];
+            segments[i] = new long[MAX_ARR_SIZE];
         }
 
-        segments[segmentCount - 1] = (E[]) new Object[(int) (size - (MAX_ARR_SIZE * (segmentCount - 1)))];
+        segments[segmentCount - 1] = new long[(int) (size - (MAX_ARR_SIZE * (segmentCount - 1)))];
     }
 
     /**
@@ -45,7 +54,7 @@ public final class OversizeArray<E> implements OversizeList<E> {
      * @param index the index of the element to be returned
      * @return the element at the supplied index
      */
-    public E get(long index) {
+    public long get(long index) {
         long segment = index / MAX_ARR_SIZE;
         long offset = index % MAX_ARR_SIZE;
         return segments[((int) segment)][((int) offset)];
@@ -56,17 +65,16 @@ public final class OversizeArray<E> implements OversizeList<E> {
      * @param index the index which will be the set to the value
      * @param value the value to be set at the index
      */
-    public void set(long index, E value) {
+    public void set(long index, long value) {
         long segment = index / MAX_ARR_SIZE;
         long offset = index % MAX_ARR_SIZE;
         segments[((int) segment)][((int) offset)] = value;
     }
 
     /**
-     * The number of elements that can be stored in this oversizecollections.OversizeIntArrayves.OversizeIntArray.
+     * The number of elements that can be stored in this OversizeIntArray.
      */
-    @Override
-    public long size() {
+    public long getSize() {
         return size;
     }
 }
