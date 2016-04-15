55d2a6389e3d73860d2eb0f69740b8a58508d362
Matthew Hebert <soyrux@gmail.com>
2016-04-15 16:50:59 -0400

    Improve sieve

--- src/oversizecollections/examples/PrimeSieve.java
+++ src/oversizecollections/examples/PrimeSieve.java
@@ -7,17 +7,23 @@ import java.text.NumberFormat;
 /**
  * Prime sieve
  *
- * Primes under 10 billion by default
+ * Primes under 1 billion by default
  */
 public class PrimeSieve {
 
     public static void main(String[] args) {
-        long primeCap = 10000000000l;
-        long primesFound = 0;
+        long startTime = System.currentTimeMillis();
 
-        OversizeBooleanArray boolArray = new OversizeBooleanArray((long) (primeCap * 1.01));
+        long primeCap = 1000000000l;
+        long primesFound = 1; // Set to one because the prime '2' will not be found by the algorithm. It is pre-marked as prime.
 
-        for (int i = 2; i < primeCap; i++) {
+        OversizeBooleanArray boolArray = new OversizeBooleanArray((long) (primeCap * 1.1));
+
+        for (long i = 0; i < Math.ceil(primeCap / 2); i++) {
+            boolArray.set(i * 2, true);
+        }
+
+        for (int i = 3; i < primeCap; i+=2) {
             if(!boolArray.get(i)) {
                 primesFound++;
 
@@ -30,5 +36,6 @@ public class PrimeSieve {
 
         NumberFormat numberFormat = NumberFormat.getInstance();
         System.out.println(numberFormat.format(primesFound) + " primes below " + numberFormat.format(primeCap));
+        System.out.println("Computation finished in: " + numberFormat.format(System.currentTimeMillis() - startTime) + "ms");
     }
 }
