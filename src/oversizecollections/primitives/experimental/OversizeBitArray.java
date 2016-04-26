e39d82ad7ffa635f2eadbc7388ec7dd57f9d64be
Matthew Hebert <soyrux@gmail.com>
2016-04-26 04:59:48 -0400

    Move OversizeBitArray to experimental package

--- src/oversizecollections/primitives/experimental/OversizeBitArray.java
+++ src/oversizecollections/primitives/experimental/OversizeBitArray.java
@@ -1,4 +1,4 @@
-package oversizecollections.primitives;
+package oversizecollections.primitives.experimental;
 
 /**
  * This class will generate an easily usable array that is larger than the maximum allowable
